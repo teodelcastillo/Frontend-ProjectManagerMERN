fb797c10b3771b691318138fda91106f654419fe
Teodoro del Castillo <teodorodelcastillop@gmail.com>
2023-09-27 20:13:59 -0300

    Optimization

--- src/hooks/caseHooks/useGetCaseWithAppointments.ts
+++ src/hooks/caseHooks/useGetCaseWithAppointments.ts
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react";
+// useCasesWithAppointments.js
+import { useState, useEffect } from "react";
 import { getCasesWithAppointments } from "../../services/casesService";
 import { Case } from "../../data/models";
 
@@ -8,54 +9,13 @@ function useCasesWithAppointments() {
     useEffect(() => {
         const fetchData = async () => {
             const casesWithAppointments = await getCasesWithAppointments();
-            // Sort the cases by urgency here
-            const sortedCases = sortCasesByUrgency(casesWithAppointments);
-            setCases(sortedCases);
+            setCases(casesWithAppointments); // Set cases directly without sorting
         };
 
         fetchData();
     }, []);
 
     return cases;
-}
-
-// Sorting function
-function sortCasesByUrgency(cases: Case[]): Case[] {
-    const now = new Date();
-    return cases.sort((a, b) => {
-        // Calculate the urgency score for each case based on your criteria
-        const urgencyScoreA = calculateUrgencyScore(a, now);
-        const urgencyScoreB = calculateUrgencyScore(b, now);
-
-        // Sort by urgency score in ascending order (change to descending if needed)
-        return urgencyScoreA - urgencyScoreB;
-    });
-}
-
-// Calculate urgency score based on your criteria (expired, today, this week, more than a week)
-function calculateUrgencyScore(caseItem: Case, now: Date): number {
-    const appointmentDates = caseItem.appointments.map((appointment) =>
-        new Date(appointment.date)
-    );
-
-    const minDistance = Math.min(
-        ...appointmentDates.map((date) => Math.abs(date.getTime() - now.getTime()))
-    );
-
-    // Define your scoring logic here based on the minimum distance
-    if (minDistance < 0) {
-        // Expired
-        return 1;
-    } else if (minDistance < 24 * 60 * 60 * 1000) {
-        // Today
-        return 2;
-    } else if (minDistance < 7 * 24 * 60 * 60 * 1000) {
-        // This week
-        return 3;
-    } else {
-        // More than a week
-        return 4;
-    }
 }
 
 export default useCasesWithAppointments;
